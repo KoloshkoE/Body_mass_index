f1b414e2b274c0139612a196ce53d8b0364585c9
Koloshko Evgeni <kolohkoe@gmail.com>
2023-01-07 11:13:30 +0700

    Fixed

--- src/BmiService.java
+++ src/BmiService.java
@@ -1,10 +1,8 @@
 public class BmiService {
-    public  double calculate(double weight, double height)
-    {
+    public double calculate(double weight, double height) {
 
-        double bmi =(weight/((height/100)*(height/100))) ;
-       return bmi;
-
+        double bmi = (weight / ((height / 100) * (height / 100)));
+        return bmi;
 
 
     }
--- src/Main.java
+++ src/Main.java
@@ -2,8 +2,8 @@ public class Main {
     public static void main(String[] args) {
         BmiService index = new BmiService();
 
-        int index_body = (int)index.calculate(65,170);
-        System.out.println("Индекс массы тела  " +index_body);
+        int indexBody = (int) index.calculate(65, 170);
+        System.out.println("Индекс массы тела  " + indexBody);
 
 
     }
